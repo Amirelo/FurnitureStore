5e411ea98b11384a1cee0dbde7ce9340b872b1aa
minhdang210799 <80700398+Amirelo@users.noreply.github.com>
2023-09-29 09:21:10 +0700

    SearchFragment: category gesture detector + addOnItemTouchListener

--- app/src/main/java/com/example/furniturestore/SearchFragment.kt
+++ app/src/main/java/com/example/furniturestore/SearchFragment.kt
@@ -1,14 +1,19 @@
 package com.example.furniturestore
 
 import android.os.Bundle
+import android.util.Log
+import android.view.GestureDetector
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.furniturestore.adapters.CategoryAdapter
 import com.example.furniturestore.models.Category
+import com.example.furniturestore.models.Product
 import com.google.android.material.appbar.MaterialToolbar
 
 class SearchFragment : Fragment() {
@@ -21,7 +26,7 @@ class SearchFragment : Fragment() {
         val rcCategories: RecyclerView = view.findViewById(R.id.rcCategories)
         val toolbar: MaterialToolbar = view.findViewById(R.id.toolbar)
 
-        // Category list
+        // temp category list
         val cate1 = Category(1, "Furniture","https://images.pexels.com/photos/890669/pexels-photo-890669.jpeg")
         val cate2 = Category(2, "Lighting","https://images.pexels.com/photos/890669/pexels-photo-890669.jpeg")
         val cate3 = Category(3, "Rugs","https://images.pexels.com/photos/890669/pexels-photo-890669.jpeg")
@@ -34,6 +39,13 @@ class SearchFragment : Fragment() {
         val cate10 = Category(10, "Boxes","https://images.pexels.com/photos/890669/pexels-photo-890669.jpeg")
 
         val listCategory: List<Category> = listOf(cate1,cate2,cate3,cate4,cate5,cate6,cate7,cate8, cate9,cate10)
+
+        // temp product list
+        val obj1 : Product = Product(1, "table", 30000.0, "Is a table","20230928", "https://images.pexels.com/photos/3705540/pexels-photo-3705540.jpeg")
+        val obj2 : Product = Product(2, "chair", 207550.0, "Is a chair","20230928", "https://images.pexels.com/photos/2762247/pexels-photo-2762247.jpeg")
+        val obj3 : Product = Product(3, "curtain", 239000.0, "Is a curtain","20230928", "https://images.pexels.com/photos/3255244/pexels-photo-3255244.jpeg")
+        val obj4 : Product = Product(4, "carpet", 121000.0, "Is a carpet","20230928", "https://images.pexels.com/photos/3705540/pexels-photo-3705540.jpeg")
+        val listProducts = listOf(obj1,obj2,obj3,obj4)
 
         val categoryAdapter = CategoryAdapter(listCategory)
 
@@ -48,6 +60,31 @@ class SearchFragment : Fragment() {
         toolbar.setNavigationOnClickListener {
             parentFragmentManager.popBackStack()
         }
+
+        rcCategories.addOnItemTouchListener(object: RecyclerView.OnItemTouchListener{
+            var gestureDetector = GestureDetector(requireContext(), object:GestureDetector.SimpleOnGestureListener() {
+                override fun onSingleTapUp(e:MotionEvent):Boolean {
+                    return true
+                }
+            })
+            override fun onInterceptTouchEvent(rv: RecyclerView, e: MotionEvent): Boolean {
+                val child = rv.findChildViewUnder(e.x, e.y)
+                if (child != null && gestureDetector.onTouchEvent(e)){
+                    val position = rv.getChildAdapterPosition(child)
+                    Toast.makeText(requireContext(), listCategory[position].name, Toast.LENGTH_SHORT).show()
+                }
+                return false
+            }
+
+            override fun onTouchEvent(rv: RecyclerView, e: MotionEvent) {
+                Log.d("-------", "Touched")
+            }
+
+            override fun onRequestDisallowInterceptTouchEvent(disallowIntercept: Boolean) {
+                TODO("Not yet implemented")
+            }
+        })
+
 
         return view
     }
