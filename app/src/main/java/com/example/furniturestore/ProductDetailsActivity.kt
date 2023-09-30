ae0e726c1ed38ddc64b739463fd5d2cf63c06646
minhdang210799 <80700398+Amirelo@users.noreply.github.com>
2023-09-30 22:19:12 +0700

    layouts: FilterActivity, StoryActivity, Popup dialog, category with checkbox item, story fill screen

--- app/src/main/java/com/example/furniturestore/ProductDetailsActivity.kt
+++ app/src/main/java/com/example/furniturestore/ProductDetailsActivity.kt
@@ -3,7 +3,7 @@ package com.example.furniturestore
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class ProductDetails : AppCompatActivity() {
+class ProductDetailsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_product_details)
