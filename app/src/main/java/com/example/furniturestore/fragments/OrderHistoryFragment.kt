1fc93e903c375c47da9e78171546ce0c5c39b4f7
minhdang210799 <80700398+Amirelo@users.noreply.github.com>
2023-10-03 16:55:26 +0700

    layouts: completed

--- app/src/main/java/com/example/furniturestore/fragments/OrderHistoryFragment.kt
+++ app/src/main/java/com/example/furniturestore/fragments/OrderHistoryFragment.kt
@@ -7,14 +7,14 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.furniturestore.R
 
-class NewAddressFragment : Fragment() {
+class OrderHistoryFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_new_address, container, false)
+        return inflater.inflate(R.layout.fragment_order_history, container, false)
     }
 
 }
